4ffb84dc07ec6d0cfff54160521dbfd9e4a2e633
veliyama <usefulbrain22@gmail.com>
2023-05-24 14:41:38 +0300

    Put and Delete methods
    Adding put and delete methods for new location

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -12,5 +12,17 @@ class Test_create_place():
         check_post = result_post.json()
         place_id = check_post.get('place_id')
 
-        print("Get method")
+        print("Get method for post method")
+        result_get: Response = Google_maps_api.get_new_place(place_id)
+
+        print('Put method')
+        result_put: Response = Google_maps_api.put_new_place(place_id)
+
+        print("Get method for put method")
+        result_get: Response = Google_maps_api.get_new_place(place_id)
+
+        print('Delete method')
+        result_put: Response = Google_maps_api.delete_new_place(place_id)
+
+        print("Get method for delete method")
         result_get: Response = Google_maps_api.get_new_place(place_id)
--- utils/api.py
+++ utils/api.py
@@ -29,7 +29,7 @@ class Google_maps_api():
         post_url = base_url + post_resource + key
         print(post_url)
         result_post = Http_method.post(post_url, json_create_new_place)
-        print('Prompt result:' + result_post.text)
+        print('Prompt result: ' + result_post.text)
         return result_post
 
     """Method for verifying of creation new location"""
@@ -40,5 +40,33 @@ class Google_maps_api():
         get_url = base_url + get_resource + key + '&place_id=' + place_id
         print(get_url)
         get_result = Http_method.get(get_url)
-        print('Prompt result' + get_result.text)
+        print('Prompt result: ' + get_result.text)
         return get_result
+
+    """Method for updating information of new location"""
+
+    @staticmethod
+    def put_new_place(place_id):
+        put_resource = '/maps/api/place/update/json'  # put method resource
+        put_url = base_url + put_resource + key + '&place_id=' + place_id
+        json_for_updating_new_location = {
+            "place_id": place_id,
+            "address": "100 Lenina street, RU",
+            "key": "qaclick123"
+        }
+        print(put_url)
+        put_result = Http_method.put(put_url, json_for_updating_new_location)
+        print('Prompt result: ' + put_result.text)
+        return put_result
+
+    @staticmethod
+    def delete_new_place(place_id):
+        delete_resource = '/maps/api/place/delete/json'  # delete method resource
+        delete_url = base_url + delete_resource + key
+        json_for_delete_new_location = {
+            "place_id": place_id,
+        }
+        print(delete_url)
+        delete_result = Http_method.delete(delete_url, json_for_delete_new_location)
+        print('Prompt result: ' + delete_result.text)
+        return delete_result
